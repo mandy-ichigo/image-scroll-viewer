301557f89ccfdfde15715a800a1d2ee3a0f2656d
mandy-ichigo <82893316+mandy-ichigo@users.noreply.github.com>
2025-04-06 13:04:28 +0530

    Delete Image Scoll Viewer directory

--- Image Scoll Viewer/scripts/domRefs.js
+++
@@ -1,9 +0,0 @@
-// domRefs.js
-export const folderPickerBtn = document.getElementById('folderPickerBtn');
-export const backBtn = document.getElementById('backBtn');
-export const bookmarkBtn = document.getElementById('bookmarkBtn');
-export const bookmarkMenu = document.getElementById('bookmarkMenu');
-export const folderList = document.getElementById('folderList');
-export const feed = document.getElementById('feed');
-export const pageIndicator = document.getElementById('pageIndicator');
-export const widthSlider = document.getElementById('widthSlider');
